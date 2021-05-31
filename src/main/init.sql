e5f045e75dcbabc71eed29de05bf385fe64deaa2
Adam97 <adamsors97@gmail.com>
2021-05-31 21:00:46 +0200

    Spring backend

--- src/main/init.sql
+++ src/main/init.sql
@@ -1,5 +1,6 @@
   
 DROP TABLE IF EXISTS items;
+DROP TABLE IF EXISTS sales;
 CREATE TABLE items(id serial PRIMARY KEY, name VARCHAR(255), price INTEGER);
 CREATE TABLE sales(id serial PRIMARY KEY, itemid INTEGER references items(id), purchasedate DATE);
 DROP SEQUENCE IF EXISTS hibernate_sequence;
